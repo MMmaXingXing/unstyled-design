93fc19869281f20c9788cee1d8c0f965a4fc31e6
Mahongxin <1172406997@qq.com>
2022-07-01 18:15:43 +0800

    feat: 新增vite中vue组件

--- packages/unstyled-design/vite.config.ts
+++ packages/unstyled-design/vite.config.ts
@@ -1,8 +1,16 @@
+import path from "path";
 import typescript2 from "rollup-plugin-typescript2";
 import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'design',
+      // the proper extensions will be added
+      fileName: 'design'
+    },
     rollupOptions: {
       external: ["vue"],
       output: {
@@ -13,6 +21,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    vue(),
     {
       ...typescript2(),
       apply: "build",
